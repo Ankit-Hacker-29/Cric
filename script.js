05db97b3299bb64a563623f809a28af71b024fdb
Ankit-Hacker-29 <luckof12323@gmail.com>
2025-02-04 22:43:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function authenticate() {
     let passcode = prompt("Enter the secure passcode:");
-
-    // Simple Passcode Validation (Change '1234' to your secure passcode)
     if (passcode === "1234") {
         document.getElementById("doc-links").style.display = "block";
         alert("Access Granted!");
     } else {
         alert("Access Denied!");
     }
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme');
 }
