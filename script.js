70d41ee5d8626c11ebc9bb2f9937513cfe170fb5
Ankit-Hacker-29 <luckof12323@gmail.com>
2025-03-09 15:33:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-const iframe = document.getElementById('myIframe');
-const main = document.getElementById('main');
-function toggleFullscreen() {
-	if (!document.fullscreenElement) {
-		// If the iframe is not currently in fullscreen mode, go fullscreen
-		iframe.requestFullscreen().catch((err) => {
-			console.error('Error attempting to enable fullscreen:', err);
-		});
-	} else {
-		// If the iframe is currently in fullscreen mode, exit fullscreen
-		document.exitFullscreen();
-	}
+function changeSource(url) {
+    document.getElementById("videoFrame").src = url;
 }
-const da = document.getElementById('da');
-da.addEventListener('click', function () {
-	if (iframe.classList[0] !== 'ab') {
-		iframe.classList.add('ab');
-		da.innerHTML = 'Enabled ads';
-		da.style.backgroundColor = 'red';
-	} else {
-		iframe.classList.remove('ab');
-		da.innerHTML = 'Disabled ads';
-		da.style.backgroundColor = 'green';
-	}
-});
